99073d45811365a26c2aaf13c758d513e20826e1
Fabian Vallon <fabian.vallon@gmx.de>
2011-09-22 16:06:39 +0200

    exceptions module

---
+++ src/basic/exceptions.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+
+#    This file is part of Shmudder.
+#
+#    Shmudder is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Shmudder is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Shmudder.  If not, see <http://www.gnu.org/licenses/>.
+
+
+class ContextError (StandardError):
+    def __init__(self, arg):
+        self.args = arg 
+        
+class PlayerError (ContextError):
+    pass
+
+class UnknownAction (PlayerError):
+    pass
+
+class ImpossibleAction (PlayerError):
+    pass
+
+# network layer
+#################################################
+
+class UnknownPlayer (PlayerError):
+    pass
+
+class BadPassword (PlayerError):
+    pass
+
+class UnknownPlayerType (PlayerError):
+    pass
+
+class PlayerExists (PlayerError):
+    pass
+
+
+# player properties
+#################################################
+
+class ImprovementNotAllowed (PlayerError):
+    pass
+
+# social
+#################################################
+
+class CharacterNotFound (PlayerError):
+    pass
+
+class NoSubscription (PlayerError):
+    pass
+
+class UnknownChannel (PlayerError):
+    pass
+
+class GroupNotFound (PlayerError):
+    pass
+
+
+# enviroment
+#################################################
+
+class NoSuchDirection (PlayerError):
+    pass
+
+class AmbigousDirection (PlayerError):
+    pass
+
+class DetailNotFound (PlayerError):
+    pass
+
+# items
+#################################################
+
+class ItemNotFound (PlayerError):
+    pass
+
+class ItemNotInUse (PlayerError):
+    pass
+
+class UnusableItem (PlayerError):
+    pass
+
+class UnsuitableBin (PlayerError):
+    pass
+
+class NotABin (PlayerError):
+    pass
+
+class UnsuitableItem (PlayerError):
+    pass
+
+class ItemReceiverNotFound (PlayerError):
+    pass
+
+class UnsupportedUseAlias (PlayerError):
+    pass
+
+class UnsupportedUnuseAlias (PlayerError):
+    pass
+
+
+# fights
+#################################################
+
+
+class CantAttackThisCharacter (PlayerError):
+    pass
+
+
+# choosey collections
+#################################################
+
+
+class UnsuitableCharacter (PlayerError):
+    pass
+
+class UnsuitableDetail (PlayerError):
+    pass
